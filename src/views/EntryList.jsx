db4eb9af607d8d487d1ab7ae0a986ad58f7fbfbf
Andrea Cleland <andreaclelan@gmail.com>
2022-05-16 20:09:01 -0700

    added a form to entryList view

--- src/views/EntryList.jsx
+++ src/views/EntryList.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import styles from '../styles.css';
 
 export default function EntryList() {
-  return <div>EntryList</div>;
+  return (
+    <section>
+      <form className={styles.entryForm}>
+        <textarea id="guestbookInput" rows="5" cols="33"></textarea>
+        <button>Add entry</button>
+      </form>
+      <h2>Latest Entries</h2>
+    </section>
+  );
 }
