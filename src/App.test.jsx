33cf4686609ff26c2211dee64e2ff3cdad11062f
Andrea Cleland <andreaclelan@gmail.com>
2022-05-18 22:04:24 -0700

    Added a simple component test to get CI to pass

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,1 +1,17 @@
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { UserProvider } from './context/UserContext';
+import App from './App';
+
+describe('App', () => {
+  it('Title should appear on page', async () => {
+    render(
+      <MemoryRouter>
+        <UserProvider>
+          <App />
+        </UserProvider>
+      </MemoryRouter>
+    );
+    screen.getByText(/guestbook/i);
+  });
+});
